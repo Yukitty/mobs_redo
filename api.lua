1a9c6cbace6ff94b8b2a44177a36db13f0f659dc
JTE <catgirl@goddess.moe>
2018-03-11 19:14:35 -0500

    Play war_cry when pathfinding, not attack.

--- api.lua
+++ api.lua
@@ -1215,7 +1215,7 @@ local smart_mobs = function(self, s, p, dist, dtime)
 			mob_sound(self, self.sounds.random)
 		else
 			-- yay i found path
-			mob_sound(self, self.sounds.attack)
+			mob_sound(self, self.sounds.war_cry)
 
 			set_velocity(self, self.walk_velocity)
 
